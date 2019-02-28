65194ac663094eb53c705d0285b0b8baff0becfe
mimorisuzuko <hrky0206@gmail.com>
2019-02-28 19:45:33 +0900

    Enable to open/close filters

--- src/components/FilterChild.jsx
+++ src/components/FilterChild.jsx
@@ -2,9 +2,18 @@ import React, { Component } from 'react';
 import _ from 'lodash';
 import Icon from './Icon';
 import autobind from 'autobind-decorator';
+import { GoTriangleDown, GoTriangleRight } from 'react-icons/go';
 import './FilterChild.scss';
 
 export default class FilterChild extends Component {
+	constructor() {
+		super();
+
+		this.state = {
+			open: false
+		};
+	}
+
 	/**
 	 * @param {Event} e
 	 */
@@ -22,38 +31,63 @@ export default class FilterChild extends Component {
 		onClick(name);
 	}
 
+	@autobind
+	onClickHeader() {
+		this.setState(({ open }) => ({
+			open: !open
+		}));
+	}
+
 	render() {
 		const {
 			props: {
 				json: { name, members, color },
 				following
-			}
+			},
+			state: { open }
 		} = this;
 
 		return (
 			<div styleName='base'>
-				<div style={{ color }} styleName='group-name'>
-					{name}
+				<div
+					style={{ color }}
+					styleName='header'
+					onClick={this.onClickHeader}
+				>
+					{open ? <GoTriangleDown /> : <GoTriangleRight />}
+					<span styleName='group-name'>{name}</span>
+					{open
+						? null
+						: _.map(members, (member, i) => {
+							return _.includes(following, member) ? (
+								<Icon name={member} size={24} key={i} />
+							) : null;
+						  })}
 				</div>
 				<div styleName='members'>
-					{_.map(members, (member) => {
-						return (
-							<div
-								onClick={this.onClick}
-								key={member}
-								data-name={member}
-								styleName='member'
-								style={{
-									opacity: _.includes(following, member)
-										? 1
-										: 0.5
-								}}
-							>
-								<Icon name={member} size={36} />
-								<span>{member}</span>
-							</div>
-						);
-					})}
+					{open
+						? _.map(members, (member, i) => {
+							return (
+								<div
+									onClick={this.onClick}
+									key={i}
+									data-name={member}
+									styleName='member'
+									style={{
+										opacity: _.includes(
+											following,
+											member
+										)
+											? 1
+											: 0.5
+									}}
+								>
+									<Icon name={member} size={36} />
+									<span>{member}</span>
+								</div>
+							);
+						  })
+						: null}
 				</div>
 			</div>
 		);
