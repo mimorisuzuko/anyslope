70531dc61fa9bd7b62ef3a609f7cfc6824741298
mimorisuzuko <hrky0206@gmail.com>
2019-06-28 14:30:52 +0900

    Removed console

--- src/reducers/anyzaka.js
+++ src/reducers/anyzaka.js
@@ -5,10 +5,8 @@ import _ from 'lodash';
 
 export default handleActions(
 	{
-		[actions.init]: (state, { payload }) => {
-			console.error(payload);
-
-			return state.addExtraBlogs(payload.extraBlogs);
+		[actions.init]: (state, { payload: { extraBlogs } }) => {
+			return state.addExtraBlogs(extraBlogs);
 		},
 		[actions.addArticles]: (state, { payload }) => {
 			_.forEach(payload, ({ length }, i) => {
